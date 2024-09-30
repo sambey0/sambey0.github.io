03a43fa71df580cb49fa5434d8e9ad6db9094ea1
samarth batra <samarthh45@gmail.com>
2024-09-29 23:44:12 -0400

    Updates

---
+++ precache-manifest.aa2bd26292c26a70a6ca65798bee155f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32839dbe704bee70107d7ddce06785d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21701c068d75167ea310",
+    "url": "/static/css/main.28bba7a7.chunk.css"
+  },
+  {
+    "revision": "62ae38ec84978f62167f",
+    "url": "/static/js/2.d64711bb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d64711bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21701c068d75167ea310",
+    "url": "/static/js/main.12f57a04.chunk.js"
+  },
+  {
+    "revision": "c36ce27fe921ac8b2570",
+    "url": "/static/js/runtime-main.622b5d1c.js"
+  }
+]);
